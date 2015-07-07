7d6e8b12d3981ea0195a71f0f33f1e4f37cb1dd6
SessionXIII <jhettjones@gmail.com>
2015-07-07 12:00:22 -0400

    First Commit
    Contains the scenes as currently planned with primitive navigation
    between each scene.

---
+++ Gemlight Alchemy/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Loads new scenes based on the scene name rather than the scene ID number
+	public void LoadScene (string sceneName){
+		Debug.Log ("Requested to load scene: " + sceneName);
+		Application.LoadLevel (sceneName);
+	}
+}
